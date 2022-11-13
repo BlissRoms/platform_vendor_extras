a9c911c49f36eaac92ad4a9a9b8825c721622bd9
minaripenguin37 <alexfinhart@gmail.com>
2022-11-13 20:10:42 +0800

    extras: Import Evolve and evo sans fonts
    taken from [1] converted to ttf.
    
    https://www.dafont.com/made-evolve-sans.font
    
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_packages.mk
+++ bliss_packages.mk
@@ -19,6 +19,8 @@ PRODUCT_PACKAGES += \
     FontCookieRunOverlay \
     FontCoolstoryOverlay \
     FontExotwoOverlay \
+    FontEvoSansOverlay \
+    FontEvolveSansOverlay \
     FontFifa2018Overlay \
     FontGrandHotelOverlay \
     FontHarmonySansOverlay \
