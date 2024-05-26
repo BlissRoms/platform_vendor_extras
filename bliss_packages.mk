0d3c257e6a1efcc7dd9e6cae8dd8f5b61e777516
Joey <joey@evolution-x.org>
2024-05-26 23:59:33 +0900

    extras: Add Cool Jazz font
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_packages.mk
+++ bliss_packages.mk
@@ -17,6 +17,7 @@ PRODUCT_PACKAGES += \
     FontComfortaaOverlay \
     FontComicSansOverlay \
     FontCookieRunOverlay \
+    FontCooljazzOverlay \
     FontCoolstoryOverlay \
     FontExotwoOverlay \
     FontEvoSansOverlay \
